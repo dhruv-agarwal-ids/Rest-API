07768a8f9124e0bf3d1ae27cf2e8ed2210f13320
dhruv-agarwal-ids <dagarwal@idssoft.com>
2024-08-06 17:12:18 +0530

    User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 // models/User.js
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -13,22 +12,5 @@ const UserSchema = new mongoose.Schema({
         required: true
     }
 });
-
-// Pre-save hook to hash the password
-UserSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) {
-        return next();
-    }
-//Generated a salt
-    const salt = await bcrypt.genSalt(10);
-//Hashing the password using salt
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
-
-// Method to compare passwords
-UserSchema.methods.matchPassword = async function(enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-};
 
 module.exports = mongoose.model('User', UserSchema);

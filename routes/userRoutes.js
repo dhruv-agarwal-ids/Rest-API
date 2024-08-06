32ae7fcdeb178d55bdffd4fbf2da4582618aabd5
dhruv-agarwal-ids <dagarwal@idssoft.com>
2024-08-06 17:10:32 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 // routes/userRoutes.js
 const express = require('express');
 const User = require('../models/User');
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
